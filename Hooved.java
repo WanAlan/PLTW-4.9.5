c2f306a510189adc1a4211082f22928088de2a71
WanAlan <160784747+WanAlan@users.noreply.github.com>
2025-03-31 13:03:52 -0700

    Update Hooved.java

--- Hooved.java
+++ Hooved.java
@@ -1,8 +1,23 @@
 /*
  * Activity 4.9.2
  */
+
 public class Hooved extends Animal
 {
+  private String food;
+  private boolean nocturnal;
+  private double aveLifeSpan;
+
+  public Hooved(String food, boolean nocturnal, double aveLifeSpan)
+  {
+    super(food, nocturnal, aveLifeSpan);
+  }
+
+  public Hooved()
+  {
+    System.out.println("A hooved animal has arrived.");
+  }
+
   public void forage()
   {
     System.out.println("The hooved animal forages for food.");
